52e5643afc97e7f3f2ed942d8cb768d214d70b04
Miloš Lukić <MilosLukic@users.noreply.github.com>
2020-03-05 22:50:18 +0100

    Renamed to Godot navigation lite.

--- godot_project/addons/detour-navigation/detour_navigation_bootstrap.gd
+++
@@ -1,8 +0,0 @@
-extends Spatial
-tool
-
-onready var to_load_script = preload("res://addons/detour-navigation/bin/detour_navigation.gdns")
-func _ready():
-	set_script(to_load_script)
-
-
---
+++ godot_project/addons/godot-navigation-lite/detour_navigation_mesh_cached_bootstrap.gd
@@ -0,0 +1,6 @@
+extends Spatial
+tool
+
+onready var to_load_script = preload("res://addons/godot-navigation-lite/bin/detour_navigation_mesh_cached.gdns")
+func _ready():
+	set_script(to_load_script)
